7d17d5ce127e42ae01fb6453b8d266e07425e197
Richard <rsberrocal@gmail.com>
2021-01-22 14:37:46 +0100

    error modules

--- common.js
+++ common.js
@@ -1,6 +1,6 @@
 const discord = require('discord.js');
 
-module.export.isUserInChannel = async (message) =>{
+module.exports.isUserInChannel = async (message) =>{
     return message.member.voice.channel;
 }
 
