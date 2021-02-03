9fddc51cb4797f9a997eb919f6e007d33a742159
Richard <rsberrocal@gmail.com>
2021-02-03 20:49:31 +0100

    cantara?

--- commands/play.js
+++ commands/play.js
@@ -9,20 +9,26 @@ const common = require('../common');
 
 // De esta forma llamamos a la funcion run desde otros archivos
 module.exports.run = async (client, message, args) => {
-    console.log('Running');
-    console.log('in chan', common.isUserInChannel(message));
     if (common.isUserInChannel(message)) {//Si esta en el canal del usuario
-        console.log('en canal');
         //const connection = await message.member.voice.channel.join();
         const link = args[0];
         console.log('link', link);
-        const songInfo = await ytdl(link)
+        const songInfo = await yt(link)
 
-        console.log(songInfo);
+        song = {
+            title: songInfo.videoDetails.title,
+            url: songInfo.videoDetails.video_url,
+            duration: songInfo.videoDetails.lengthSeconds,
+            thumbnail: songInfo.videoDetails.thumbnail.thumbnails[3].url
+        };
+
+        const connection = await message.member.voice.channel.join();
+        const dispatcher = connection.connection
+            .play(yt(song.url))
+        console.log(song);
         console.log('connected');
-
+        return message.channel.send(`Escuchando ${song.title}`);
     } else {
-        console.log('no en canal');
         return message.channel.send('No estas en un canal de voz');
     }
 }
